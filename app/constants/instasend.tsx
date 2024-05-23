dd0fef56a8a7be7a1c5e16678f5d59840a0a5d8e
karlcolins141 <karlcolins141@gmail.com>
2024-05-23 20:45:35 +0000

    first commit

---
+++ app/constants/instasend.tsx
@@ -0,0 +1,15 @@
+'use client';
+import { Button } from '@nextui-org/button'
+
+export default function MyScreen(){
+
+    async function test (){
+  }
+return(
+  <div>
+  	<form action={test}>
+      <Button >Pay Now</Button>
+    </form>
+  </div>
+)
+}
